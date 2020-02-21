16183d2dd5016ecd490b884c625413eec8bfd2e1
Alex <barajasr@sonoma.edu>
2020-02-21 13:50:12 -0800

    help me

--- proj01.py
+++ proj01.py
@@ -1,4 +1,4 @@
-# Alex Barajas-Ritchie 
+# Alex Barajas-Ritchie
 # Alondra Lona
 # CS415 Project01
 
@@ -11,20 +11,26 @@ def main ():
     main()
 
 
+#task 1
 
 def fib (k):
-
+    # compute the kth fibonacci number
       if (k <= 1):
          return k
       else:
          return fib(k-1) + fib(k-2)
 
-#print (fib(int(input())))
+print (fib(int(input("Compute the kth fib number: "))))
 
 def fib2(k):
+    # calculate corresponding number of additions
     if (k == 0 or k == 1):
         f=0
     else:
         f = fib2(k-1) + fib2(k-2) + 1
     return f
-print(fib2(int(input())))
+print(fib2(int(input("Compute the number of additions A(k): "))))
+
+
+
+# task 2
