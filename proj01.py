373167ad0db29edbf67211058871f7d40a81d4c7
Alondra Lona <Alondra@MacBook-Pro-5.local>
2020-02-21 12:57:30 -0800

    fib Works

--- proj01.py
+++ proj01.py
@@ -1,4 +1,23 @@
 # Alex Barajas-Ritchie 
 # Alondra Lona
-# CS415 Project01 
-~                   
+# CS415 Project01
+
+import math
+
+def main ():
+
+    #print(fib(3))
+
+    main()
+
+
+
+def fib (k):
+
+      if (k <= 1):
+         return k
+      else:
+         return fib(k-1) + fib(k-2)
+
+print (fib(int(input())))
+
