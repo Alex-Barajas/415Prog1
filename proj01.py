247165a1028d291eb0190103098f2f0e4a698044
Alondra Lona <Alondra@MacBook-Pro-5.local>
2020-02-21 14:16:46 -0800

    works now

--- proj01.py
+++ proj01.py
@@ -1,14 +1,10 @@
 # Alex Barajas-Ritchie
 # Alondra Lona
+# test test test
 # CS415 Project01
 
 import math
 
-def main ():
-
-    #print(fib(3))
-
-    main()
 
 
 #task 1
@@ -20,7 +16,8 @@ def fib (k):
       else:
          return fib(k-1) + fib(k-2)
 
-print (fib(int(input("Compute the kth fib number: "))))
+
+
 
 def fib2(k):
     # calculate corresponding number of additions
@@ -29,7 +26,14 @@ def fib2(k):
     else:
         f = fib2(k-1) + fib2(k-2) + 1
     return f
-print(fib2(int(input("Compute the number of additions A(k): "))))
+
+
+def main():
+    print(fib(int(input("Compute the kth fib number: "))))
+    print(fib2(int(input("Compute the number of additions A(k): "))))
+
+
+main()
 
 
 
